9bd9d5bd9a1f623d206033d0cea73a00210951dc
Krunal Patel <Pkrunal616@gmail.com>
2019-12-05 20:05:16 -0500

    chaning port and adding start

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const server = require('./api/server.js');
 
 
-const port = 4000;
+const port =  process.env.PORT || 8000;
 
 server.listen(port, () => {
     console.log(`\n Server is runing on localhost:${port} \n`)
